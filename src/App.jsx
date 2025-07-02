dc060bff01b6a3646c523cbad78563669e660a9f
Romain Al <romainal@gmail.com>
2025-07-02 14:46:16 +0200

    change dpr

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,18 @@
-import { Bvh, PerformanceMonitor } from "@react-three/drei";
+import { Bvh } from "@react-three/drei";
 import { Canvas } from "@react-three/fiber";
 import { EcctrlJoystick } from "ecctrl";
 import { Perf } from "r3f-perf";
-import { Suspense, useState } from "react";
+import { Suspense } from "react";
 import Experience from "./Experience";
 
 export default function App() {
-  const [dpr, setDpr] = useState(2);
-  console.log("🚀 ~ App ~ dpr:", dpr);
+  // const [dpr, setDpr] = useState(2);
+  // console.log("🚀 ~ App ~ dpr:", dpr);
   return (
     <>
       <EcctrlJoystick />
       <Canvas
-        dpr={2}
+        dpr={1.5}
         gl={{ antialias: true }}
         shadows
         camera={{
@@ -29,14 +29,14 @@ export default function App() {
       >
         <color args={["black"]} attach="background" />
         {/* <OrbitControls /> */}
-        <PerformanceMonitor onIncline={() => setDpr(2)} onDecline={() => setDpr(1)}>
-          <Suspense fallback={null}>
-            <Bvh firstHitOnly>
-              <Experience />
-            </Bvh>
-          </Suspense>
-          <Perf position="top-left" />
-        </PerformanceMonitor>
+        {/* <PerformanceMonitor onIncline={() => setDpr(2)} onDecline={() => setDpr(1)}> */}
+        <Suspense fallback={null}>
+          <Bvh firstHitOnly>
+            <Experience />
+          </Bvh>
+        </Suspense>
+        <Perf position="top-left" />
+        {/* </PerformanceMonitor> */}
       </Canvas>
     </>
   );
